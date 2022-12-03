a4f9ff67482cff5e020590a86bae9ef2c1964a1e
dave <6188353@cgodin.qc.ca>
2022-12-02 20:34:51 -0500

    ajouter PageChoisirService.js

--- src/composants/Main.js
+++ src/composants/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PageChoisirService from './PageChoisirService'
 import PageConnexion from './PageConnexion'
 import PageRegister from './PageRegister'
 
@@ -6,8 +7,9 @@ export default function Main() {
   
   return (
     <div id='idMain'>
-{/*     <PageConnexion/>   */}
-      <PageRegister/>
+{/*     <PageConnexion/>   
+      <PageRegister />   */}
+      <PageChoisirService />
     </div>
   )
 }
