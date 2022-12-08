4287d95a30974599428943f7d4ec30752f302eec
Tech Potentials <techpotentials@gmail.com>
2022-12-07 23:07:43 -0500

    New: PageVosReservations & ReservationCard

--- src/composants/Main.js
+++ src/composants/Main.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PageConnexion from './PageConnexion';
 
 import PageReservation from './PageReservation';
+import PageVosReservations from './PageVosReservations';
 
 export default function Main() {
   
   return (
     <div id='idMain'>
-      <PageReservation/>
+      <PageVosReservations/>
     </div>
   )
 }
