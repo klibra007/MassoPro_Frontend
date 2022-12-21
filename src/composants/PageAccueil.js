22012587da8cf311841c411f3e7c08e5442cdcec
saubenas <108959190+saubenas@users.noreply.github.com>
2022-12-21 13:49:37 -0500

    petite maj

--- src/composants/PageAccueil.js
+++ src/composants/PageAccueil.js
@@ -9,17 +9,17 @@ export default function PageAccueil() {
     const navigate = useNavigate();
 
     console.log(localStorage.getItem('connexionData'));
-    console.log("resultat dans accueil : "+ resultat.idClient)
+    console.log("resultat dans accueil : " + resultat.idClient)
 
     const handleClickReserver = () => {
-        if(resultat !== null){
+        if (resultat !== null) {
             navigate('/reservation');
-            
+
         }
     }
 
     const handleClickConnecter = () => {
-        if(resultat !== null){
+        if (resultat !== null) {
             navigate('/connexion');
         }
     }
@@ -28,45 +28,51 @@ export default function PageAccueil() {
         if (resultat.idClient !== null && resultat.idClient !== undefined) {
             return <div>
                 {/* <b>PageAccueil Client</b> */}
-                <h2>Bonjour {resultat.prenom}!</h2><br/><br/>
+                <h2>Bonjour {resultat.prenom}!</h2><br /><br />
                 {/* <p style={{ fontWeight: "bold" }}>Token : {resultat.token.substr(3)}</p> */}
-                <Button style={{ backgroundColor: "#a98467" ,borderColor: "#a98467",width: "300px" }}  onClick={handleClickReserver}>Réserver un rendez-vous</Button><br/><br/>
-               
-                <h1> Nos massages coup de coeur </h1> 
+                <Button style={{ backgroundColor: "#a98467", borderColor: "#a98467", width: "300px" }} onClick={handleClickReserver}>Réserver un rendez-vous</Button><br /><br />
+
+                <h1> Nos massages coup de coeur </h1>
                 <div id='services'>
-
+                <div id='idBloc1'>
                     <div id='therap1'>
-                    
-                    </div>
-                    <div id='therapText'>
-                    <h1> Massage thérapeutique </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
-                    </div>
 
-                    <div id='therapText'>
-                    <h1> Massage Suédois </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                    </div>
+                    <div id='therapText1'>
+                        <h1> Massage thérapeutique </h1>
+                        <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                    </div>
+                </div>
+                <div id='idBloc2'>
+                <div id='therapText2'>
+                        <h1> Massage Suédois </h1>
+                        <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
                     </div>
                     <div id='therap2'>
-                    
+
                     </div>
+                </div>
+                <div id='idBloc3'>   
 
                     <div id='therap3'>
-                    
-                    </div>
-                    <div id='therapText'>
-                    <h1> Massage pierres chauffantes </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
-                    </div>
 
-                    <div id='therapText'>
-                    <h1> Massage Suédois </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                    </div>
+                    <div id='therapText3'>
+                        <h1> Massage pierres chauffantes </h1>
+                        <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                    </div>
+                </div>
+                <div id='idBloc4'>
+                <div id='therapText4'>
+                        <h1> Massage Suédois </h1>
+                        <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
                     </div>
                     <div id='therap4'>
-                    
+
                     </div>
-                   
+                </div>
+                    
+
                 </div>
             </div>
         } else if (resultat.idPersonnel !== null && resultat.idPersonnel !== undefined) {
@@ -77,59 +83,63 @@ export default function PageAccueil() {
             </div>
         } else if (resultat.idAdministrateur !== null && resultat.idAdministrateur !== undefined) {
             return <div>
-                <b>PageAccueil Admin</b> 
+                <b>PageAccueil Admin</b>
                 <p>Bonjour {resultat.prenom}, vous êtes connecté!</p>
                 <p>Token : {resultat.token.substr(3)}</p>
             </div>
         } else {
-            return <div id='idTestmain'>
-                <div id='idTestmain1'>
+            return <div>
                 
-                </div>
-                <div id='idTestmain2'>
-                <p style={{ fontWeight: "bold",fontSize:"20px" }}> La massothérapie est l’une des plus anciennes thérapies existantes pour traiter ou soulager les douleurs physiques. Elle réunit différentes techniques manuelles pour relaxer les groupes musculaires.</p>
-                <Button 
-                style={{ backgroundColor: "#a98467" ,borderColor: "#a98467" }} 
-                onClick={handleClickConnecter}>Prendre un rendez-vous
-                </Button><br/><br/>
-                <h1> Nos massages coup de coeur </h1> 
-                <div id='services'>
+                <div id='idImageAccueilNonConnecte'></div>
+                <div id='idAccueilNonConnecte'>
+                    <p style={{ fontWeight: "bold", fontSize: "20px" }}> La massothérapie est l’une des plus anciennes thérapies existantes pour traiter ou soulager les douleurs physiques. Elle réunit différentes techniques manuelles pour relaxer les groupes musculaires.</p>
+                    <Button
+                        style={{ backgroundColor: "#a98467", borderColor: "#a98467" }}
+                        onClick={handleClickConnecter}>Prendre un rendez-vous
+                    </Button><br /><br />
+                    <h1> Nos massages coup de coeur </h1>
+                    <div id='services'>
+                    <div id='idBloc1'>
+                       <div id='therap1'>
 
-                    <div id='therap1'>
-                    
-                    </div>
-                    <div id='therapText'>
-                    <h1> Massage thérapeutique </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
-                    </div>
+                       </div>
+                       <div id='therapText1'>
+                        <h1> Massage thérapeutique </h1>
+                        <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                       </div>
 
-                    <div id='therapText'>
-                    <h1> Massage Suédois </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
                     </div>
-                    <div id='therap2'>
-                    
-                    </div>
+                    <div id='idBloc2'>
+                        <div id='therapText2'>
+                            <h1> Massage Suédois </h1>
+                            <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                        </div>
+                        <div id='therap2'>
 
+                        </div>
+                    </div>
+                    <div id='idBloc3'>
                     <div id='therap3'>
-                    
+
+</div>
+<div id='therapText3'>
+    <h1> Massage pierres chauffantes </h1>
+    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+</div>
                     </div>
-                    <div id='therapText'>
-                    <h1> Massage pierres chauffantes </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                    <div id='idBloc4'>
+                    <div id='therapText4'>
+                            <h1> Massage Suédois </h1>
+                            <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
+                        </div>
+                        <div id='therap4'>
+
+                        </div>
                     </div>
 
-                    <div id='therapText'>
-                    <h1> Massage Suédois </h1>
-                    <p> Massage en profondeur ciblé sur les tensions et douleurs ou sur un besoin spécifique. </p>
                     </div>
-                    <div id='therap4'>
-                    
-                    </div>
-                   
                 </div>
-                </div>
-                
+
             </div>
         }
     }
