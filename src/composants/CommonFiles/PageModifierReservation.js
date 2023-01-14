f691a8d925306869c0325dab2a4cdac870fb3efb
dave <6188353@cgodin.qc.ca>
2023-01-14 15:36:50 -0500

    ajouter validation PageChoixDureeMasso.js

--- src/composants/CommonFiles/PageModifierReservation.js
+++ src/composants/CommonFiles/PageModifierReservation.js
@@ -3,38 +3,52 @@ import { Modal, Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import axios from 'axios';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { selectObjReservation, setObjetReservationIdPersonnel, setNomMassoChoisi } from '../../app/features/reservationSlice';
+import { isOnlyNumber } from '../../lib/FormValidator';   
+import { render } from '@testing-library/react';
 
 export default function PageModifierReservation(props) {
     const pageName="PageModifierReservation";
-    const {data, show, setShow, callbackFunc} = props;
-
     const noDispoMsg="Aucun disponibilité pour cette date";
 
-    const [dureeTab, setDureeTab] = useState([]);
-    const [massoTab, setMassoTab] = useState([]);
-    const [disponibiliteTab, setDisponibiliteTab] = useState([]);
-    //const objReservation = useSelector(selectObjReservation);
-    //const dispatch = useDispatch();
+    const {data, show, setShow, callbackFunc} = props;
 
-    const [duree, setDuree] = useState('');
-    const [masso, setMasso] = useState('');
-    const [dateRes, setDateRes] = useState('');
-    const [heureRes, setHeureRes] = useState('');    
+    const [dureeTab, setDureeTab] = useState([]);
+    const [massoTab, setMassoTab] = useState([]);  
+    const [disponibiliteTab, setDisponibiliteTab] = useState([]);    
+    
+    const formInitState = {
+        idDuree: data.idDuree,
+        idPersonnel: data.idPersonnel,
+        dateRes: data.dateRes,
+        heureDebut: data.heureDebut
+    }
+
+    const [form, setform] = useState({
+        idDuree: '',
+        idPersonnel: '',
+        dateRes: '',
+        heureDebut: ''
+    }) 
+    
+    const [ formErrors, setFormErrors ] = useState({})
 
     let strDossierServeur = "https://dev.pascalrocher.com";
     let strAppDuree = strDossierServeur + "/api/durees";    
     let strAppPersonnels = strDossierServeur + "/api/servicespersonnels";
     let strAppRdv = strDossierServeur + "/api/rendezvous";
-
+   
     useEffect(() => {
         //recup des durées
         axios.get(strAppDuree)
             .then((response) => {
                 //console.log("PageModifierReservation. La réponse durée : " + JSON.stringify(response.data));
-                setDureeTab(response.data);
+                if (response.data.length > 0) {
+                   // If there are data 
+                   setDureeTab(response.data);
+                } else {
+                   // no data, set array empty  
+                   setDureeTab([]);                    
+                }
             })
             .catch(error => alert(error));   
 
@@ -44,144 +58,166 @@ export default function PageModifierReservation(props) {
         axios.get(strAppUrl)
             .then((response) => {
                 //console.log("PageModifierReservation. La réponse masso : " + JSON.stringify(response.data));
-                setMassoTab(response.data);
+                if (response.data.length > 0) {
+                    // If there are data 
+                    setMassoTab(response.data);
+                } else {
+                    // no data, set array empty  
+                    setMassoTab([]);                      
+                }
             })
             .catch(error => alert(error));               
-                    
-    }, [data]) 
+                              
+    }, [data])     
 
     const initForm = () => {
-       console.log("Inside initForm. idPersonnel="+data.idPersonnel);
-       setDuree(data.idDuree);
-       setMasso(data.idPersonnel);
-       setDateRes(data.dateRes);
-
-       getDisponibilite(); 
-       console.log("Dispo length="+disponibiliteTab.length);
+       setform(formInitState);
+       //console.log("idDuree="+form.idDuree);
+       //console.log("disponibiliteTab.length="+disponibiliteTab.length);
+       getDisponibilite();
     }
 
     const getDisponibilite = () => {
-       let objReservation = {
-          "date": dateRes,
-          "idService": data.idService,
-          "idPersonnel": masso,
-          "idDuree": duree
-       }
-
-       console.log(pageName+" "+JSON.stringify(objReservation));
-       axios.post(strAppRdv, JSON.stringify(objReservation), {
-        headers: {
-            'Content-Type': 'application/json'
+        let objReservation = {
+           "date": form.dateRes,
+           "idService": data.idService,
+           "idPersonnel": form.idPersonnel,
+           "idDuree": form.idDuree
         }
-    })
-        .then((response) => {
-          console.log(pageName+" La réponse: " + JSON.stringify(response));
-          if (response.data.length > 0) {
-            setDisponibiliteTab(response.data);
-          } else {
-            setDisponibiliteTab([]);
-          }
+ 
+        console.log(pageName+" "+JSON.stringify(objReservation));
+        axios.post(strAppRdv, JSON.stringify(objReservation), {
+           headers: {
+             'Content-Type': 'application/json'
+           }
         })
-        .catch((error) => {
-          alert(error.response);
-        });       
-    }
-    
-    const handleChangeDuree = (event) => {    
-        const duree = event.target.value;
-        setDuree(duree);
-        //console.log(pageName+" Le duree choisi: id="+duree);
-    }     
-    
-    const handleChangeMasso = (event) => {    
-        const masso = event.target.value;
-        setMasso(masso);
-        //console.log(pageName+" Le masso choisi: id="+masso);
-    }   
-    
-    const handleChangeDateRes = (event) => {    
-        const dateRes = event.target.value;
-        setDateRes(dateRes);
-        console.log(pageName+" Le date choisi: "+dateRes);
-    }      
+         .then((response) => {
+           console.log(pageName+" La réponse: " + JSON.stringify(response));
+           if (response.data.length > 0) {
+              setDisponibiliteTab(response.data);
+           } else {
+              setDisponibiliteTab([]);
+           }
+         })
+         .catch((error) => {
+           alert(error.response);
+         });       
+    }    
 
-    const handleChangeHeureRes = (event) => {    
-        const heure = event.target.value;
-        setHeureRes(heure);
-        console.log(pageName+" Le heure choisi: "+heure);
-    }  
+    const validateForm = () => {
+        const errors = {};
 
-    // isConfirm=true (Button Confirm clicked). False=Button Cancel clicked
-    function onModalClose(isConfirm) {
+        //console.log("form.idDuree="+form.idDuree);
+        if (form.idDuree == 0) {   // No selection. Value is 0
+           errors.idDuree = "Veuillez choisir une durée";
+        }   
+        if (form.idPersonnel == 0) {   // No selection. Value is 0
+           errors.idPersonnel = "Veuillez choisir un massothérapeute";
+        }          
+
+        return errors;
+    }    
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        const frmErrors = validateForm();
+        if (Object.keys(frmErrors).length > 0) {
+            setFormErrors(frmErrors);
+        } else {
+            setFormErrors({});  // Reset error fields
+            setShow(false);
+            callbackFunc("data"); 
+        }        
+    }    
+    
+    const handleCancelForm = (e) => {
         setShow(false);
-        if (isConfirm) {
-           callbackFunc("data"); 
-        }
-    }
+    }   
+
+    const handleChange = (e) => {
+       setform({ ...form, [e.target.name]: e.target.value })
+
+       // Check and see if errors exist, and remove them from the error object
+       if (!!formErrors[e.target.name]) setFormErrors({
+          ...formErrors,
+          [e.target.name]: null
+       })        
+    }    
 
     return (
-      <Modal 
-         show={show}
-         onHide={() => setShow(false)}  
-         onShow={initForm}  
-         backdrop="static"
-         animation={false}    
-      >
-        <Modal.Header closeButton>
-            <Modal.Title>Modifier vos réservation</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+        <Modal 
+           show={show}
+           onHide={() => setShow(false)}   
+           onShow={initForm}
+           backdrop="static"
+           animation={false}    
+        >
+          <Modal.Header closeButton>
+              <Modal.Title>Modifier vos réservation</Modal.Title>
+          </Modal.Header>    
+          <Modal.Body>          
             <Form>
-              <div>Numéro de réservation: {data.reservation}</div>
-              <div>Duree: {duree}</div>
-              <div>idPersonnel: {masso}</div>
+               <div>Numéro de réservation: {data.reservation}</div>   
 
-              <div className='text-start mt-2'>Durée</div>
-              <Form.Select id='idDuree' defaultValue={data.idDuree} onChange={(e) => { handleChangeDuree(e) }}>
-                    <option value={0}>Veuillez choisir une durée svp</option>
-                    {dureeTab.map((data) => {
+               <Form.Group>
+                  <div className='text-start mt-2'>Durée</div>
+                  <Form.Select id='idDuree' name="idDuree" value={form.idDuree} 
+                     onChange={handleChange} isInvalid={!!formErrors.idDuree}>
+                     <option value={0}>Veuillez choisir une durée svp</option>
+                     {dureeTab.map((data) => {
                         const { id, duree, prix, estActif } = data;
                         if (estActif === 1) {
                             return <option value={id} key={id}>
                                 {`${duree}min (+ $${prix})`}</option>
                         }
-                    })}
-              </Form.Select>              
+                     })}
+                  </Form.Select>  
+                  <Form.Control.Feedback type="invalid" className='text-start'>
+                     {formErrors.idDuree}
+                  </Form.Control.Feedback>                   
+                </Form.Group>
 
-              <div className='text-start mt-2'>Massothérapeute</div>
-              <Form.Select id='idMasso' defaultValue={data.idPersonnel} onChange={(e) => { handleChangeMasso(e) }}>
-                    <option value={0}>Veuillez choisir un massothérapeute svp</option>
-                    {massoTab.map((masso) => {
-                      return <option key={masso.id} value={masso.id}>
-                         {`${masso.prenom} ${masso.nom}`}</option>
-                })}
-              </Form.Select>
+                <Form.Group>
+                  <div className='text-start mt-2'>Massothérapeute</div>
+                  <Form.Select id='idPersonnel' name="idPersonnel" value={form.idPersonnel} 
+                       onChange={handleChange} isInvalid={!!formErrors.idPersonnel}>
+                     <option value={0}>Veuillez choisir un massothérapeute svp</option>
+                     {massoTab.map((masso) => {
+                        return <option key={masso.id} value={masso.id}>
+                          {`${masso.prenom} ${masso.nom}`}</option>
+                     })}
+                  </Form.Select>
+                  <Form.Control.Feedback type="invalid" className='text-start'>
+                     {formErrors.idPersonnel}
+                  </Form.Control.Feedback>                                        
+                </Form.Group> 
 
-              <div className='text-start mt-2'>Choisir une date réservation</div>                
-              <InputGroup>
-                  <Form.Control type='text' id="idDateRes" value={dateRes} onChange={(e) => handleChangeDateRes(e)} required />
+                <div className='text-start mt-2'>Choisir une date réservation</div>   
+                <InputGroup>
+                  <Form.Control type='text' id="idDateRes" name="dateRes" value={form.dateRes} 
+                      onChange={handleChange} required />
                   <Button id="idDateResBtn" variant="secondary" onClick={() => getDisponibilite()}>Changer Date</Button>                 
-              </InputGroup>
-                  
-              {disponibiliteTab.length > 0 ? <Form.Select className="mt-2" id='idDateResSel' onChange={(e) => { handleChangeHeureRes(e) }}>
+                </InputGroup> 
+                {disponibiliteTab.length > 0 ? <Form.Select className="mt-2" id='idHeureDebut' name="heureDebut" onChange={handleChange}>
                     <option value={0}>Veuillez choisir un disponibilité</option>
                     {disponibiliteTab.map((dispo) => {
                       return <option key={dispo.heureDebut} value={dispo.heureDebut}>
                          {dispo.heureDebut}</option>
                 })}
               </Form.Select>
-              : <div className='text-center mt-2'>{noDispoMsg}</div>}
+              : <div className='text-center mt-2'>{noDispoMsg}</div>}                                             
             </Form>
-            
-        </Modal.Body>     
-        <Modal.Footer>
-            <Button variant="outline-secondary" onClick={() => onModalClose(false)}>
+          </Modal.Body>     
+          <Modal.Footer>
+             <Button variant="outline-secondary" onClick={handleCancelForm}>
                 Annuler
-            </Button>
-            <Button variant="primary" onClick={() => onModalClose(true)}>
+             </Button>
+             <Button variant="primary" onClick={handleSubmitForm}>
                 Modifier ma réservation
-            </Button>                
-        </Modal.Footer>
-      </Modal>
-    )
+             </Button>                
+          </Modal.Footer>
+        </Modal>
+    )   // end return 
+  
 }  // end function
